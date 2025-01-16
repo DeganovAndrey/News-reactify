12c360f1e33c1456ad68c5c652a083e8920df8bf
DeganovAndrey <Deganovdyoosha@yandex.ru>
2025-01-16 15:17:32 +0300

    ready app

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import s from "./Header.module.css";
+import { formateDate } from "../helpers/formatDate";
+
+const Header = () => {
+  return (
+    <header className={s.header}>
+      <h1 className={s.title}>NEWS REACTIFY</h1>
+      <p className={s.date}>{formateDate(new Date())}</p>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -0,0 +1,17 @@
+import s from "./NewsBanner.module.css";
+import { formatTimeAgo } from "../helpers/formatTimeAgo";
+import Image from "./../Image/Image";
+
+const NewsBanner = ({ item }) => {
+  return (
+    <div className={s.banner}>
+      <Image image={item?.image} />
+      <h3 className={s.title}>{item.title}</h3>
+      <p className={s.extra}>
+        {formatTimeAgo(item.published)} by {item.author}
+      </p>
+    </div>
+  );
+};
+
+export default NewsBanner;
